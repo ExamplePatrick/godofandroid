092615edf2327ce9110c7ae8761d45970c4aaf11
ExamplePatrick <broadcast.programmer@gmail.com>
2016-06-28 15:05:58 +0900

    Pacakge names are changed.

--- MyApplication/daemonservice/src/main/java/com/example/daemonservice/MainActivity.java
+++ MyApplication/daemonservice/src/main/java/com/example/daemonservice/MainActivity.java
@@ -1,10 +1,12 @@
-package com.example.myapplication;
+package com.example.daemonservice;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
---
+++ MyApplication/remoteservice/src/main/aidl/com/example/myapplication/RemoteStub.aidl
@@ -0,0 +1,9 @@
+// RemoteStub.aidl
+package com.example.myapplication;
+
+// Declare any non-default types here with import statements
+
+interface RemoteStub {
+
+    String getServerPackageName();
+}
