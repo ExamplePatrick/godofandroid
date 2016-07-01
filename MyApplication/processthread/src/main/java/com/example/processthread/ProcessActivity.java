dc8f7e8fe8d0c615333a7722657d00db9fb53692
ExamplePatrick <broadcast.programmer@gmail.com>
2016-07-01 16:13:47 +0900

    process thread

---
+++ MyApplication/processthread/src/main/java/com/example/processthread/ProcessActivity.java
@@ -0,0 +1,16 @@
+package com.example.processthread;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProcessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_process);
+
+
+
+    }
+}
