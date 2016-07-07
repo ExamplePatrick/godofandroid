cb311aa40604fd269d4b5440295de1aba91fdd72
ExamplePatrick <broadcast.programmer@gmail.com>
2016-07-07 15:52:40 +0900

    Relative Layout

--- AndroidUI/app/src/main/java/com/example/androidui/MainActivity.java
+++ AndroidUI/app/src/main/java/com/example/androidui/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout1);
+        setContentView(R.layout.relative_layout3);
     }
 }
